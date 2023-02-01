9f5d3aca37517fff91a54fd1f217a614567cc866
Singularity Software <124126097+singularitysoft@users.noreply.github.com>
2023-02-01 08:41:40 -0500

    Updated refactorCode function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,31 @@
 <script>
   import { apiKey } from './apiKey.js';
+  import axios from 'axios';
 
   let code = '';
 
-  async function refactorCode(code) {
-    const response = await fetch('https://api.openai.com/v1/engines/gpt-3/jobs', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer API_KEY',
-      },
-      body: JSON.stringify({
-        prompt: `Refactor the following code:\n${code}\n`,
-      }),
-    });
-    const result = await response.json();
-    console.log(result);
+  async function refactorCode() {
+    try {
+      const response = await axios({
+        method: 'post',
+        url: 'https://api.openai.com/v1/edits',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`
+        },
+        data: {
+          "model": "text-davinci-edit-001",
+          "input": "my input",
+          "instruction": "instruction"
+        }
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   }
+
+  refactorCode();
 </script>
 
 <h1>One Click Code Refactor</h1>
