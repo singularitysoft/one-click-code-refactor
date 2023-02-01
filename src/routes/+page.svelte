ace38aac8906d656e1f1b8aee8c7bcfbfdcc0b30
Singularity Software <124126097+singularitysoft@users.noreply.github.com>
2023-02-01 09:11:17 -0500

    added styling to the ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,6 +28,66 @@
   refactorCode();
 </script>
 
-<h1>One Click Code Refactor</h1>
-<textarea bind:value={code}></textarea>
-<button on:click={refactorCode}>Refactor Code</button>
+<div class="container">
+  <h1>One Click Code Refactor</h1>
+  <textarea bind:value={code}></textarea>
+  <button on:click={refactorCode}>Refactor Code</button>
+</div>
+
+<style>
+  .container {
+    background-color: #333;
+    color: #fff;
+    font-family: sans-serif;
+    padding: 20px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h1 {
+    text-align: center;
+    margin-top: 0;
+    margin-bottom: 20px;
+  }
+
+  textarea {
+    width: 375px;
+    height: 130px;
+    background-color: #555;
+    color: #fff;
+    border: none;
+    padding: 10px;
+    font-size: 16px;
+  }
+
+  @media (max-width: 440px) {
+    textarea {
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 300px) {
+    textarea {
+      width: 160px;
+    }
+  }
+
+  @media (max-width: 250px) {
+    textarea {
+      width: 100%;
+    }
+  }
+
+  button {
+    display: block;
+    margin: 20px auto;
+    margin-bottom: 5px;
+    padding: 10px 20px;
+    background-color: #fff;
+    color: #333;
+    border: none;
+    cursor: pointer;
+  }
+</style>
